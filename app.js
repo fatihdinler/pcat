5a5d5d39f08e3cd6215693468f4bcd5ad70cffd2
fatihdinler <fatihdinler458@gmail.com>
2023-03-02 00:42:08 +0300

    [main] added prettier, nodemon and etc.

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const fn = () => {
-  console.log('first')
-}
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+
+  const photo = {
+    id : 1,
+    name : 'Car photo',
+    description : 'This is an excelent car photo.'
+  }
+
+  res.send(photo)
+})
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`Server is started at localhost:${port}`)
+})
