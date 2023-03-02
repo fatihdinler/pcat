27ab0e730186b25fd73463ed5650d0ef9b9c430f
fatihdinler <fatihdinler458@gmail.com>
2023-03-02 13:40:58 +0300

    [main] created ejs modules, views and partials folders with routing, etc.

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const path = require('path')
+const ejs = require('ejs')
 
 const app = express()
+
+// Template Engine
+app.set('view engine', 'ejs')
 
 // Middleware...
 app.use(express.static('public'))
 
+// Routing
 app.get('/', (req, res) => {
-  let file = path.resolve(__dirname, 'temp/index.html')
-  res.sendFile(file)
+  res.render('index')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/add', (req, res) => {
+  res.render('add')
 })
 
 const port = 3000
